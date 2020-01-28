73dcee6c3d48a281d6142e6689b4882e69688e45
ImpMK <imp_mk@naver.com>
2020-01-28 23:27:02 +0900

    Updates

---
+++ precache-manifest.0daeced99ee751e1e8b7a3f647095580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad0dd63ddde448d0f6998e8b4f9d88f",
+    "url": "/lastorigin-index/index.html"
+  },
+  {
+    "revision": "c9e149d1401830cd00ba",
+    "url": "/lastorigin-index/static/css/main.684e5283.chunk.css"
+  },
+  {
+    "revision": "9147957c3a01ded931aa",
+    "url": "/lastorigin-index/static/js/2.681d0fd7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lastorigin-index/static/js/2.681d0fd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9e149d1401830cd00ba",
+    "url": "/lastorigin-index/static/js/main.b4d745ed.chunk.js"
+  },
+  {
+    "revision": "c510c74973af027cf8f0",
+    "url": "/lastorigin-index/static/js/runtime-main.4675a46b.js"
+  }
+]);
